c216bbae2cb573b15224a37435bad112f0fdc88c
zobirofkir <zobirofkir19@gmail.com>
2024-11-12 11:52:57 +0100

    create users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'image',
+        'is_admin',
+        'is_active',
     ];
 
     /**
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_active')->default(true);
+            $table->string('image')->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
