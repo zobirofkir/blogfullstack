c834b0b162ac0e55b9cca619e66beebea8349ce2
zobirofkir <zobirofkir19@gmail.com>
2024-11-13 14:18:27 +0100

    list blogs in home page

--- resources/views/components/blogs.blade.php
+++ resources/views/components/blogs.blade.php
@@ -3,17 +3,17 @@
         <div class="max-w-full mx-auto rounded-lg overflow-hidden my-10 ">
             <h1 class="text-3xl font-bold text-white mb-4">Blogs</h1>
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                @foreach ([1, 2, 3, 4, 5, 6, 7, 8] as $item)
-                <a href="#">
+                @foreach ($blogs->take(8) as $blog)
+                <a href="{{route('blogs.show', $blog->slug)}}">
                     <div class="relative w-full group">
                         <img
-                            src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg"
+                            src="{{ asset('storage/' . $blog->images[0]) }}"
                             alt="Nature Image"
-                            class="w-full h-full object-cover rounded-lg border-white border-2"
+                            class="w-full h-64 object-cover rounded-lg border-white border-2"
                         />
                         <div class="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent p-4 opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300">
-                            <h2 class="text-white text-2xl font-semibold">Stunning Nature Scenery</h2>
-                            <p class="text-white text-sm mt-1">Explore the beauty of nature in its purest form.</p>
+                            <h2 class="text-white text-2xl font-semibold">{{$blog->title}}</h2>
+                            <p class="text-white text-sm mt-1">{{Str::limit($blog->description, 80)}}</p>
                         </div>
                     </div>
                 </a>
