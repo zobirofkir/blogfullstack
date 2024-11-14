d44113ac1a101d3fb36d73daa1abb385b851e16c
zobirofkir <zobirofkir19@gmail.com>
2024-11-14 08:27:12 +0100

    remove title and description

--- resources/views/pages/categories/show.blade.php
+++ resources/views/pages/categories/show.blade.php
@@ -1,8 +1,6 @@
 <x-app-layout>
     <div class="container mx-auto px-6 py-8 flex flex-col lg:flex-row items-center justify-center bg-gray-50 rounded-lg shadow-xl">
         <img src="{{ asset('storage/' . $category->image) }}" alt="{{ $category->title }}" class="w-[300px] h-[300px] shadow-lg hover:animate-spin transition duration-300 object-cover rounded-full">
-        <h1 class="text-4xl font-extrabold text-center mt-6 text-gray-800">{{ $category->title }}</h1>
-        <p class="text-gray-700 mt-3 text-lg text-center">{{ $category->description }}</p>
     </div>
 
     <div class="container mx-auto px-4 py-8 flex flex-col lg:flex-row">
