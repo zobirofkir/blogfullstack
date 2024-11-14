cf70abfa893c2c37b705a3da73b8e0270d35c4b8
zobirofkir <zobirofkir19@gmail.com>
2024-11-14 12:26:35 +0100

    change bg-color for selected category from bar-category component

--- resources/views/components/bar-categories.blade.php
+++ resources/views/components/bar-categories.blade.php
@@ -1,5 +1,6 @@
 @php
     $categories = App\Services\Facades\CategoryFacade::get()['categories'];
+    $currentSlug = request()->segment(2);
 @endphp
 
 <div class="container mx-auto mt-[80px] md:px-0 px-6">
@@ -7,7 +8,10 @@
         @foreach ($categories as $category)
             <div>
                 <a href="{{ url('/categories', $category->slug) }}">
-                    <h1 class="text-md font-bold bg-blue-200 px-8 py-4 rounded-lg  my-10">{{ $category->title }}</h1>
+                    <h1 class="text-md font-bold px-8 py-4 rounded-lg my-10
+                        {{ $category->slug === $currentSlug ? 'bg-blue-500 text-white' : 'bg-blue-200' }}">
+                        {{ $category->title }}
+                    </h1>
                 </a>
             </div>
         @endforeach
