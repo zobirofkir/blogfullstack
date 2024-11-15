5136537a32cfb751220f9faa6e74d967d9d2c5ae
zobirofkir <zobirofkir19@gmail.com>
2024-11-15 12:43:35 +0100

    add longText from description in categorie

--- database/migrations/2024_11_12_175759_create_categories_table.php
+++ database/migrations/2024_11_12_175759_create_categories_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId("user_id")->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->string("title");
             $table->string("image");
-            $table->text("description");
+            $table->longText("description");
             $table->string("slug")->unique();
             $table->timestamps();
         });
